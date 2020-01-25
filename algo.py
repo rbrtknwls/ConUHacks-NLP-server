2d61045b257a4db8d9e2f361a39f648a4cd34a39
Robert Knowles <robbie.knowles@ucc.on.ca>
2020-01-25 18:46:48 -0500

    POS Tagger intigration

--- algo.py
+++ algo.py
@@ -3,12 +3,8 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import RegexpTokenizer
+from nltk import pos_tag
 
-import spacy
-from spacy import displacy
-from collections import Counter
-import en_core_web_sm
-nlp = en_core_web_sm.load()
 
 
 stopWords = set(stopwords.words('english'))
@@ -16,8 +12,10 @@ tokenizer = RegexpTokenizer(r'\w+')
 
 
 
-yeet = "TODAY I WOKE UP AND HAD SOME COFFEE. Next I walked to school. I SAW TOM!!!! YAY"
+yeet = "I walked to google. I talked to TOM. I got work"
 
+
+# FUNCTIONS THAT RUN BEFORE TO CLEAN CODE
 def preproc(text):
         final = []
         text = tokenizer.tokenize(text.lower());
@@ -27,19 +25,33 @@ def preproc(text):
                 final.append(n);
         return final
     
-    
-def findpnouns(text):
-    doc = nlp(text);
-    for x in doc.ents:
-        print(x.text);
-    
+# QUERY CODE
+def classifynouns(text):
+    final = []
+    for tup in text:
+        if (tup[1][0] == "N"): 
+            final.append(tup[0])
+    return final
+
+def classifyverbs(text):
+    final = []
+    for tup in text:
+        if (tup[1][0] == "V"): 
+            final.append(tup[0])
+    return final
+
 def start(text):
     
     phara = sent_tokenize(text)
     for rawsent in phara:
+        print("-------")
+        sent = pos_tag(preproc(rawsent))
         
-        propnoun = findpnouns(rawsent)
-        #sent = preproc(rawsent)
+        
+        print ("finding nouns!")
+        print(classifynouns(sent));
+        print ("finding verbs!")
+        print(classifyverbs(sent));
         #doc = nlp("Google is");
         #print([(X.text, X.label_) for X in doc.ents])
 
