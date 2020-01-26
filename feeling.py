403342396b36cfc5811e7b250ed8e0ccc0904f15
Robert Knowles <robbie.knowles@ucc.on.ca>
2020-01-25 19:11:12 -0500

    finished feeling

--- feeling.py
+++ feeling.py
@@ -14,7 +14,7 @@ tokenizer = RegexpTokenizer(r'\w+')
 sid = SentimentIntensityAnalyzer()
 
 
-yeet = "I hated work today. I talked to TOM. I got work"
+yeet = "I hated work today. I loved talking to TOM. I got work"
 
 
 # FUNCTIONS THAT RUN BEFORE TO CLEAN CODE
@@ -28,19 +28,39 @@ def preproc(text):
         return final
     
 # QUERY CODE
+def intentfram (sent):
+    score = sid.polarity_scores(sent);
+    mat = 100* score['pos'] -100 * score['neg']
+    return mat
 
+
+
+def display (z):
+    for x in z:
+        print(x)
+        
+        
 def start(text):
+    #JANK
+    inde = []
+    total = 0;
+    count = 0;
+    
     
     phara = sent_tokenize(text)
     for rawsent in phara:
-        print("-------")
+        count += 1;
         sent = pos_tag(preproc(rawsent))
-        
-        
-        print(sid.polarity_scores(rawsent));
-        #doc = nlp("Google is");
-        #print([(X.text, X.label_) for X in doc.ents])
+        inde.append(intentfram(rawsent));
+        total += intentfram(rawsent);
+    
+    total = total/count;
+    print("SENTANCE SCORE:")
+    display(inde);
+    print("TOTAL SCORE:")
+    print(total)
 
+    
 
 
 start(yeet)
